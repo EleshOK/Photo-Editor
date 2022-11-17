79328f3e80701961b88113c5216c1418e71ef36b
Eduard Dob <dobacedik@gmail.com>
2022-11-17 20:00:28 +0300

    Поменял расположение кнопок, и добавил blur

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Window():
     def __init__(self):
         self.display = Tk()
         self.display.title('Photo editor')
-        self.display.geometry('600x600')
+        # self.display.geometry('600x630')
         self.display.resizable(width=0, height=0)
         self.display.columnconfigure(4, minsize=30)
         self.display.configure(borderwidth=0)
@@ -21,26 +21,32 @@ class Window():
         # buttons and more
 
         self.button_upload_photo = Button(self.display, text='Upload', command=self.open_file)
-        self.button_upload_photo.grid(row=2, columnspan=4)
+        self.button_upload_photo.grid(row=3, columnspan=4)
         
         self.button_rotate_left = Button(self.display, text='Rotate left', command=self.rotate_left)
-        self.button_rotate_left.grid(row=0, column=2)
+        self.button_rotate_left.grid(row=1, column=2)
 
         self.button_rotate_right = Button(self.display, text='Rotate right', command=self.rotate_right)
-        self.button_rotate_right.grid(row=0, column=3)
+        self.button_rotate_right.grid(row=1, column=3)
         
-        self.text_bright = Label(self.display, text="Brightness:", font=("ariel 10 bold"))
+        self.text_bright = Label(self.display, text="Brightness:", font=("Arial 10 bold"))
         self.text_bright.grid(row=0, column=0)
 
         self.v2 = IntVar()
+        self.v1 = IntVar()
 
         self.scale2 = ttk.Scale(self.display, from_=0, to=20, variable=self.v2,
                    orient=HORIZONTAL, command=self.brightness)
         
+        self.scale_blur = ttk.Scale(self.display, from_=0, to=20, variable=self.v1, orient=HORIZONTAL, command=self.blur)
+        self.text_blur = Label(self.display, text='Blur:', font=('Arial 10 bold'))
+        self.text_blur.grid(row=0, column=2)
+        
+        self.scale_blur.grid(row=0, column=3)
         self.scale2.grid(row=0, column=1)
 
         self.canvas = Canvas(self.display, width=600, height=530, highlightthickness = 0)
-        self.canvas.grid(row=1, columnspan=4)
+        self.canvas.grid(row=2, columnspan=4)
         self.current_rotation = 0
         self.direction_rotation = None
         self.image = None
@@ -55,26 +61,25 @@ class Window():
     def open_file(self):
 
         self.img_path = filedialog.askopenfilename(initialdir=os.getcwd())
-        img = Image.open(self.img_path)
-        img.thumbnail((600, 600))
-        self.image = ImageTk.PhotoImage(img)
-        self.canvas.create_image(300, 250, image=self.image)
-        self.canvas.image = self.image
+        self.image = Image.open(self.img_path)
+        self.image.thumbnail((600, 600))
+        photo_image = ImageTk.PhotoImage(self.image)
+        self.canvas.create_image(300, 250, image=photo_image)
+        self.canvas.image = photo_image
 
     def rotate_image(self):
         if not self.direction_rotation:
             return
-        img = Image.open(self.img_path)
-        img.thumbnail((600, 600))
+        self.image.thumbnail((600, 600))
         if self.direction_rotation == 'Left':
             self.current_rotation += 10
-            img6 = img.rotate(self.current_rotation)
+            self.image = self.image.rotate(self.current_rotation)
         elif self.direction_rotation == 'Right':
             self.current_rotation -= 10
-            img6 = img.rotate(self.current_rotation)
-        self.image = ImageTk.PhotoImage(img6)
-        self.canvas.create_image(300, 210, image=self.image)
-        self.canvas.image = self.image
+            self.image = self.image.rotate(self.current_rotation)
+        photo_image = ImageTk.PhotoImage(self.image)
+        self.canvas.create_image(300, 210, image=photo_image)
+        self.canvas.image = photo_image
 
 
     def rotate_left(self):
@@ -89,10 +94,18 @@ class Window():
         for m in range(0, self.v2.get()+1):
             self.image.thumbnail((600, 600))
             imgg = ImageEnhance.Brightness(self.image)
-            img2 = imgg.enhance(m)
-            self.image = ImageTk.PhotoImage(img2)
-            self.canvas.create_image(300, 210, image=self.image)
-            self.canvas.image = self.image
+            self.image = imgg.enhance(m)
+            photo_image = ImageTk.PhotoImage(self.image)
+            self.canvas.create_image(300, 210, image=photo_image)
+            self.canvas.image = photo_image
+    
+    def blur(self, event):
+        for m in range(0, self.v1.get()+1):
+            self.image.thumbnail((600, 600))
+            imgg = self.image.filter(ImageFilter.BoxBlur(m))
+            photo_image = ImageTk.PhotoImage(imgg)
+            self.canvas.create_image(300, 210, image=photo_image)
+            self.canvas.image = photo_image
 
 
     
